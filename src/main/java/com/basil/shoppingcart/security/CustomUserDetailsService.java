0a8c9e3d115d5f5a37003f88f3ac36cc1c740621
Basil <basilpaulose58@gmail.com>
2026-07-13 15:53:01 +0530

    implemented CustomerUserDetailsService

---
+++ src/main/java/com/basil/shoppingcart/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.basil.shoppingcart.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.basil.shoppingcart.model.User;
+import com.basil.shoppingcart.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found with email : " + email));
+
+        return new UserPrincipal(user);
+    }
+
+}
